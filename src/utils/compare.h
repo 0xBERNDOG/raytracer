ca173a49ffb2c743ba961e7d096b2056d8693c90
Steven Burnett <2800058+0xBERNDOG@users.noreply.github.com>
2024-11-05 23:32:44 +0000

    Update fuzzy compare

--- src/utils/compare.h
+++ src/utils/compare.h
@@ -3,7 +3,7 @@
 
 #include <math.h>
 
-#define FUZZY_EQUALS_TOLERANCE 0.00001
-#define FUZZY_EQUALS(x, y) (fabs(x - y) <= 0.00001)
+#define FUZZY_EQUALS_TOLERANCE 1e-10
+#define FUZZY_EQUALS(x, y) (fabs(x - y) < FUZZY_EQUALS_TOLERANCE)
 
 #endif
