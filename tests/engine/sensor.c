5d37e3c5c522bc863299bdbe99af095bac810f75
Steven Burnett <2800058+0xBERNDOG@users.noreply.github.com>
2024-11-10 20:40:32 +0000

    Add sensor test (no hits)

--- tests/engine/sensor.c
+++ tests/engine/sensor.c
@@ -47,7 +47,6 @@ test_empty_scene()
 	for (size_t i = 0; i < 100; i++) {
 		mu_assert("sensor data doesn't match expected",
 		          vector_equals(expected[i], result[i]));
-		break;
 	}
 
 	free(result);
@@ -86,12 +85,51 @@ test_basic_scene()
 	return 0;
 }
 
+static char *
+test_no_hits()
+{
+	struct object *world = calloc(10, sizeof(struct object));
+	mu_assert("failed to allocate memory for world", world != NULL);
+
+	struct sensor_params params = {
+		.plane = { .position = { 0.0, 0.0, 0.0 },
+		           .normal = { 1.0, 0.0, 0.0 } },
+		.width = 10,
+		.height = 10,
+		.pixel_spacing = 1.0
+	};
+
+	struct plane plane = { .normal = { 0.0, 0.0, 1.0 },
+		               .position = { 0.0, 0.0, 20.0 } };
+	world[0] = create_plane(&plane);
+
+	struct vector expected[100];
+	for (size_t i = 0; i < 100; i++) {
+		expected[i].x = 0.0;
+		expected[i].y = 0.0;
+		expected[i].z = 0.0;
+	}
+
+	struct vector *result = sensor_capture(params, world, 0);
+	mu_assert("no data returned by sensor", result != NULL);
+
+	for (size_t i = 0; i < 100; i++) {
+		mu_assert("sensor data doesn't match expected",
+		          vector_equals(expected[i], result[i]));
+	}
+
+	free(result);
+
+	return 0;
+}
+
 char *
 test_sensor_all()
 {
 	// run all sensor tests
 	mu_run_test(test_no_scene);
 	mu_run_test(test_empty_scene);
+	mu_run_test(test_no_hits);
 	mu_run_test(test_basic_scene);
 	return 0;
 }
